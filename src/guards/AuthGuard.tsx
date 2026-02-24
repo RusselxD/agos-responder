29caf90a3eb154ee7d15f1afd596768ed381046a
RusselxD <russelcabigquez8@gmail.com>
2026-02-25 04:35:35 +0800

    fix

--- src/guards/AuthGuard.tsx
+++ src/guards/AuthGuard.tsx
@@ -1,6 +1,5 @@
 import type { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
-import { usePushNotifications } from "../hooks/usePushNotifications";
 
 interface AuthGuardProps {
     children: ReactNode;
@@ -10,8 +9,6 @@ export default function AuthGuard({ children }: AuthGuardProps) {
     if (localStorage.getItem("responderId") === null) {
         return <Navigate to="/verify" replace />;
     }
-
-    usePushNotifications();
 
     return <div>{children}</div>;
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,13 @@
 import Page from "../../components/common/Page";
+import { usePushNotifications } from "../../hooks/usePushNotifications";
 import BlockageStatusCard from "./components/BlockageStatusCard";
 import FusionAnalysisCard from "./components/FusionAnalysisCard";
 import WaterLevelStatusCard from "./components/WaterLevelStatusCard/WaterLevelStatusCard";
 import WeatherConditionCard from "./components/WeatherConditionCard/WeatherConditionCard";
 
 export default function Home() {
+    usePushNotifications();
+
     return (
         <Page>
             <FusionAnalysisCard />
