4bcd7593e073bbb095a2c4a6a149943767a70185
RusselxD <russelcabigquez8@gmail.com>
2026-05-04 11:58:38 +0800

    fix routings

--- src/guards/InstalledGuard.tsx
+++ src/guards/InstalledGuard.tsx
@@ -1,6 +1,6 @@
-// import { useEffect, useState } from "react";
-// import { Navigate, useLocation } from "react-router-dom";
-// import { isPwaInstalled } from "../lib/utils/pwa";
+import { useEffect, useState } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { isPwaInstalled } from "../lib/utils/pwa";
 import type { ReactNode } from "react";
 
 interface InstalledGuardProps {
@@ -8,22 +8,21 @@ interface InstalledGuardProps {
 }
 
 export default function InstalledGuard({ children }: InstalledGuardProps) {
-    // const [installed, setInstalled] = useState<boolean>(() => isPwaInstalled());
-    // const location = useLocation();
+    const [installed, setInstalled] = useState<boolean>(() => isPwaInstalled());
+    const location = useLocation();
 
-    // useEffect(() => {
-    //     // React to display-mode changes (e.g. user installs while on page)
-    //     const mediaQuery = window.matchMedia(
-    //         "(display-mode: standalone), (display-mode: fullscreen), (display-mode: minimal-ui)",
-    //     );
-    //     const handler = () => setInstalled(isPwaInstalled());
-    //     mediaQuery.addEventListener("change", handler);
-    //     return () => mediaQuery.removeEventListener("change", handler);
-    // }, []);
+    useEffect(() => {
+        const mediaQuery = window.matchMedia(
+            "(display-mode: standalone), (display-mode: fullscreen), (display-mode: minimal-ui)",
+        );
+        const handler = () => setInstalled(isPwaInstalled());
+        mediaQuery.addEventListener("change", handler);
+        return () => mediaQuery.removeEventListener("change", handler);
+    }, []);
 
-    // if (!installed) {
-    //     return <Navigate to="/install" state={{ from: location }} replace />;
-    // }
+    if (!installed) {
+        return <Navigate to="/install" state={{ from: location }} replace />;
+    }
 
     return <>{children}</>;
 }
