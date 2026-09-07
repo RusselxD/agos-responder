3675e96c5c3e6db546edeb157b0f543fd1d390f4
RusselxD <russelcabigquez8@gmail.com>
2026-09-07 11:17:08 +0800

    asdf

--- src/pages/InstallGate/components/Header.tsx
+++ src/pages/InstallGate/components/Header.tsx
@@ -1,13 +1,13 @@
 export default function Header() {
-    return (
-        <div className="w-full flex flex-col items-center text-center">
-            <img src="/patrol.svg" className="w-20 mb-8" alt="Patrol" />
+  return (
+    <div className="w-full flex flex-col items-center text-center">
+      <img src="/patrol.svg" className="w-20 mb-8" alt="Patrol" />
 
-            <p className="font-bold text-3xl">Install Patrol to Continue</p>
-            <p className="font-medium mt-2 text-gray-700 dark:text-gray-500">
-                Patrol must be installed on your home screen to receive push
-                notifications.
-            </p>
-        </div>
-    );
+      <p className="font-bold text-3xl text-primary dark:text-accent">Install Patrol to Continue</p>
+      <p className="font-medium mt-2 text-gray-700 dark:text-gray-500">
+        Patrol must be installed on your home screen to receive push
+        notifications.
+      </p>
+    </div>
+  );
 }
